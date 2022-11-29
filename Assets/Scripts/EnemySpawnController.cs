e86dd87a719e4ffa2fc1d4404ac040f433007a6e
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-11-29 01:31:15 -0300

    inicio da criação do spawn de inimigos

---
+++ Assets/Scripts/EnemySpawnController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+    public float positionX;
+    public float positionMaxY;
+    public float positionMinY;
+    private float positionY;
+
+    public GameObject enemy;
+
+    void Start()
+    {
+        SpawnEnemy(enemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy(GameObject enemy)
+    {
+        positionY = Random.Range(positionMinY, positionMaxY);
+        Instantiate(enemy, new Vector3(positionX, positionY, 0), Quaternion.identity);
+    }
+}
