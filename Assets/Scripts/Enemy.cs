ba303c20fdbb8bb1b3287e263bf9a594e52dfb94
1801570700 <gabrielmoura.aluno@unipampa.edu.br>
2022-12-12 23:06:36 -0300

    Script da vida do inimigo e dano do player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public int maxLife;
+    public int currencieLife; 
 
+    private void Start(){
+        currencieLife = maxLife;
+    }
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,5 +21,11 @@ public class Enemy : MonoBehaviour
         rb.velocity = new Vector2(-2f, 0f);
     }
 
-    
+    public void Damage(){
+        currencieLife = currencieLife - 1;
+
+            if(currencieLife <= 0){
+                Destroy(gameObject);
+            }
+    }
 }
--- Assets/Scripts/LifeEnemy.cs
+++ Assets/Scripts/LifeEnemy.cs
@@ -1,29 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-public class LifeCharacter : MonoBehaviour
+
+public class LifeEnemy : MonoBehaviour
 {
     public int maxLife;
     public int currencieLife;
-
-    void Start()
+        void Start()
     {
         currencieLife = maxLife;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void Damage(){
         currencieLife = currencieLife - 1;
 
             if(currencieLife <= 0){
-                SceneManager.LoadScene("EndGameScene");
+                Destroy(gameObject);
             }
     }
-
 }
